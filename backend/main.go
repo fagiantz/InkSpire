5d6dd08203432dcf032552591f87652c1887fdea
faizbastomi <64179402+FaizBastomi@users.noreply.github.com>
2026-04-27 13:52:54 +0700

    forgot the PORT

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/fagiantz/InkSpire/backend/api"
 	"github.com/fagiantz/InkSpire/backend/database"
@@ -15,10 +16,14 @@ func main() {
 
 	database.ConnectDB()
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	r := api.SetupRouter()
 
-	log.Println("Starting Gin server on :8080...")
-	if err := r.Run(":8080"); err != nil {
+	log.Printf("Starting Gin server on :%s\n", port)
+	if err := r.Run(":" + port); err != nil {
 		log.Fatalf("Server forced to shutdown with error: %v", err)
 	}
 }
